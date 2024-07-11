c13b9a4df5c00af91917dffdbb59d8575bd4a7bc
Gidk-g <devhaxel@gmail.com>
2024-07-11 23:50:28 +0200

    Update Random.hx

--- src/modules/Random.hx
+++ src/modules/Random.hx
@@ -3,10 +3,7 @@ package modules;
 import Math;
 
 class Random {
-    private static var _seed:Float = Math.random();
-
     public static function nextInt(min:Int, max:Int):Int {
-        _seed = (_seed * 1103515245.0 + 12345.0) % 0x7fffffff;
-        return min + Math.floor(_seed / 0x7fffffff * (max - min + 1));
+        return min + Std.random(max - min + 1);
     }
 }
