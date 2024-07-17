ee73ff9a6b1c96a5c6c91e9e5122c2a81b53a7dd
Gidk-g <devhaxel@gmail.com>
2024-07-17 18:52:10 +0200

    Update Flow.hx

--- src/Flow.hx
+++ src/Flow.hx
@@ -77,10 +77,10 @@ class Flow {
       error.report('No project.json file found in the current directory.');
       return;
     }
-
+  
     var jsonData = File.getContent("project.json");
     var projectData:Dynamic = Json.parse(jsonData);
-
+  
     var mainScriptPath = projectData.src + "/" + projectData.main;
     if (!FileSystem.exists(mainScriptPath)) {
       error.report('Main script "${mainScriptPath}" does not exist.');
