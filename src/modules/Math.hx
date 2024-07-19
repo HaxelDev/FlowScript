83988e97d61ba1203165a30814c11468000ccc70
Gidk-g <devhaxel@gmail.com>
2024-07-19 03:26:21 +0200

    Update Math.hx

--- src/modules/Math.hx
+++ src/modules/Math.hx
@@ -1,6 +1,10 @@
 package modules;
 
 class Math {
+    public static function getPI():Float {
+        return 3.141592653589793;
+    }
+
     public static function abs(x:Float):Float {
         return x < 0 ? -x : x;
     }
@@ -19,5 +23,29 @@ class Math {
 
     public static function sqrt(x:Float):Float {
         return Math.sqrt(x);
+    }
+
+    public static function sin(x:Float):Float {
+        return Math.sin(x);
+    }
+
+    public static function cos(x:Float):Float {
+        return Math.cos(x);
+    }
+
+    public static function tan(x:Float):Float {
+        return Math.tan(x);
+    }
+
+    public static function asin(x:Float):Float {
+        return Math.asin(x);
+    }
+
+    public static function acos(x:Float):Float {
+        return Math.acos(x);
+    }
+
+    public static function atan(x:Float):Float {
+        return Math.atan(x);
     }
 }
